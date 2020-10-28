969e71bfd31d512e4ea22bf5463d67942a47c072
walaha2002 <68665730+walaha2002@users.noreply.github.com>
2020-10-27 19:00:34 -0500

    - Adds SQL for question 4 of homework

---
+++ EmployeeSQL/2.2Q4.sql
@@ -0,0 +1,5 @@
+select e.emp_no, e.last_name, e.first_name,d.dept_name
+from employees as e, departments as d, dept_emp
+where e.emp_no=dept_emp.emp_no
+and dept_emp.dept_no = d.dept_no
+order by d.dept_name, e.last_name,e.first_name;
