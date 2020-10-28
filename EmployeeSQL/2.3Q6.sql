7633362729222c0cba1f0cea714134c343774eb9
walaha2002 <68665730+walaha2002@users.noreply.github.com>
2020-10-27 19:31:59 -0500

    - Adds SQL for question 6 of homework

---
+++ EmployeeSQL/2.3Q6.sql
@@ -0,0 +1,6 @@
+select e.emp_no, e.last_name,e.first_name,d.dept_name
+from employees as e, departments as d, dept_emp
+where e.emp_no=dept_emp.emp_no
+and dept_emp.dept_no=d.dept_no
+and dept_name = 'Sales'
+order by e.last_name,e.first_name;
